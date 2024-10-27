d5a96c21a5e904689ed941ffa821b51f7087b893
Vlad Solomahin <vlad.good47@mail.ru>
2024-10-28 00:36:14 +0300

    creating container GameOver

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const clearBtn = document.getElementById('clear');
 const backspaceBtn = window.document.getElementById('backspace');
 const enterBtn = window.document.getElementById('enter');
-const screen = window.document.querySelector('.game__content-input');
-const calc = document.querySelector('.game__content-calcBtn');
+const screenInput = window.document.querySelector('.game__content-input');
+const calcBtn = document.querySelector('.game__content-calcBtn');
 const score = window.document.getElementById('span-score');
-const ocean = document.querySelector('.game__content-ocean');
+const oceanCreateDrop = document.querySelector('.game__content-createDrop');
+const ocean = document.querySelector('.ocean');
 const startBtn = document.querySelector('.container-btnStart');
-const containerStart = document.querySelector('.container');
+const btnPlayAgain = document.querySelector('.container-btnGameOver');
+const btnMainMenu = document.querySelector('.container-btnMainMenu');
+const containerStart = document.querySelector('.containerStartGame');
+const containerGameOver = document.querySelector('.containerGameOver');
 const containerGameDrop = document.querySelector('.game');
+const imgContent = document.querySelector('.game__content-img')
+
 
 
 
@@ -16,24 +22,36 @@ const containerGameDrop = document.querySelector('.game');
 function onClickNum(event) {
     if (event.target.classList.contains('num')) {
         const pressNum = event.target.textContent
-        screen.value += pressNum
+        screenInput.value += pressNum
     } 
 }
 
 clearBtn.addEventListener('click', function(event) {
-    screen.value = ''
+    screenInput.value = ''
 })
 
 backspaceBtn.addEventListener('click', function(event) {
-    screen.value = screen.value.slice(0, -1)
+    screenInput.value = screenInput.value.slice(0, -1)
 })
 
+score.innerHTML = 0;
+let count = 0;
 enterBtn.addEventListener('click', function(event) {
-    score.textContent = screen.value
-    screen.value = ''
+    const drops = document.querySelectorAll('.drop')
+    drops.forEach((el) => {        
+        if (el.dataset.result === screenInput.value) {
+            el.remove()
+            screenInput.value = ''
+            score.textContent = +score.textContent + 10;
+            } else {
+                score.textContent = score.textContent - 5;
+                screenInput.value = ''
+            }
+        }
+    )
 })
 
-calc.addEventListener('click', function(event) {
+calcBtn.addEventListener('click', function(event) {
     onClickNum(event)
 })
 
@@ -42,39 +60,61 @@ calc.addEventListener('click', function(event) {
 startBtn.addEventListener('click', function(event) {
     containerStart.style.display = 'none'
     containerGameDrop.style.display = 'block'
-    
-    setInterval(() => {
-        createDrop()
-    }, 6000)
+    containerGameOver.style.display = 'none'
+    const drop = createDrop()
 
+    setInterval(() => {
+        const drop = createDrop()  
+    }, 8000)
+
+    setInterval(waterUp, 1000);
 });
 
-// function ranromOperation(el1, el2, el3, el4) {
-//     const oper = getRandomIntInclusive(0, 10)
-//     let sym = ('+', '-', '/', '*')
-//     function randSymbl() {
+btnPlayAgain.addEventListener('click', function(event) {
+    containerGameOver.style.display = 'none'
+    containerStart.style.display = 'none'
+    containerGameDrop.style.display = 'block'
+    const drop = createDrop()
 
-//         if (oper <= 2) {
-//             el4 
-//         }
-//         else if (oper > 2 <= 4) {
-//             el3
-//         }
-//         else if (oper > 4 <= 7) {
-//             el2
-//         }
-//         else if (oper > 7 <= 10) {
-//             el1
-//         }
-//     }
-// return randSymbl()
-// }
-// console.log(ranromOperation('+', '-', '/', '*'));
+    setInterval(() => {
+        const drop = createDrop()  
+    }, 8000)
 
+    setInterval(waterUp, 1000);
+});
+
+btnMainMenu.addEventListener('click', function(event) {
+    containerGameOver.style.display = 'none'
+    containerGameDrop.style.display = 'none'
+    containerStart.style.display = 'block'
+});
+
+
+ // Рандомный оператор
+function randomOperation() {
+    sum = '+';
+    subtract = '-';
+    multiply = '*';
+    divide = '/';
+    const oper = getRandomIntInclusive(1, 4)
+         if (oper == 1) {return sum}
+    else if (oper == 2) {return subtract}
+    else if (oper == 3) {return multiply}
+    else if (oper == 4) {return divide}
+}
+
+
+    // const rootDiv = document.getElementsByClassName("game__content-img")
+
+const currentTask = {
+    firstNum: '',
+    operation: '',
+    secondNum: '',
+}
 
 
 function createDrop(array) {
-    const rootDiv = document.getElementsByClassName("game__content-img")
+    // const rootDiv = document.getElementsByClassName("game__content-img")
 
         const drop = document.createElement("div");
         const firstNum = document.createElement("div");
@@ -82,52 +122,173 @@ function createDrop(array) {
         const secondNum = document.createElement("div");
 
         drop.classList.add('drop');
-        rootDiv[0].appendChild(drop)
+
+        // firstNum = randomExpression(1, 10);
+        // secondNum = randomExpression(1, 10)
+
+        // rootDiv[0].appendChild(drop)
+
+        if (score.textContent < 50) {
+            currentTask.operation = randomOperation(1, 2)
+        } else {
+            currentTask.operation = randomOperation(1, 4);
+        }
+        if (count > 7) {
+            drop.style.animation = 'dropAnimation 10s linear'
+        }
+        if (count > 15) {
+            drop.style.animation = 'dropAnimation 8s linear';
+        }
 
     const x = getRandomIntInclusive(0, 530)
     const y = getRandomIntInclusive(0)
     drop.style.top = y +'px'
     drop.style.left = x +'px'
-    ocean.append(drop)
+    oceanCreateDrop.append(drop)
 
-    const {firstNums, secondNums, operations} = randomExpression(0, 10)
+
+    // currentTask.firstNum = randomExpression(1, 10);
+    // currentTask.secondNum = randomExpression(1, 10);
+    // console.log(currentTask.firstNum)
+
+    const {firstNums,operations, secondNums} = randomExpression(1, 10) // operations
     firstNum.innerText = firstNums
-    secondNum.innerText = secondNums
     operation.innerText = operations
+    secondNum.innerText = secondNums
 
-    const result = +firstNum.innerText + +secondNum.innerText
+
+    
+const result = resultDropEquations()
+
+function resultDropEquations() {
+switch(operations) {
+   case '+':
+    return firstNums+secondNums;
+   case '-':
+    return firstNums-secondNums;  
+   case '*':
+    return firstNums*secondNums;
+  case '/':
+    return firstNums/secondNums;
+  }
+}
+
     drop.dataset.result = result
+    console.log(result);
 
     drop.appendChild(firstNum)
     drop.appendChild(operation)
     drop.appendChild(secondNum)
-
+    
     return drop
 }
 
+// Функция условие для корректных уравнений
 function randomExpression(min, max) {
     let firstNums = getRandomIntInclusive(min, max)
     let secondNums = getRandomIntInclusive(min, max)
-
-    const operat = Math.random() > 0.5 ? '+' : '-'
-
-    const operations = operat
+    let operations = randomOperation()
 
     if (firstNums < secondNums && (operations === '-' || operations === '/')) {
         [firstNums, secondNums] = [secondNums, firstNums]
     }
-
     if (operations === '/' && firstNums % secondNums != 0) {
         firstNums -= firstNums % secondNums
     }
+
     return {firstNums, secondNums, operations}
 }
 
+  // Рандомное значение
 function getRandomIntInclusive(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
-  }
+}
+
+
+
+
+// let trueResult = ''; // возможно не надо
+// function checkResult() {
+
+//     let trueResult = '';
+//     if (currentTask.operation === '+') {
+//         trueResult = currentTask.firstNum + currentTask.secondNum
+//     }
+
+//     if (currentTask.operation === '-') {
+//         if (currentTask.secondNum > currentTask.number1) {
+//             trueResult = currentTask.secondNum - currentTask.firstNum
+//         } else {
+//             trueResult = currentTask.firstNum - currentTask.secondNum
+//         }
+//     }
+
+//     if (currentTask.operation === '*') {
+//         trueResult = currentTask.firstNum * currentTask.secondNum
+//     }
+
+//     if (currentTask.operation === '/') {
+//         if (currentTask.secondNum > currentTask.firstNum) {
+//             trueResult = currentTask.secondNum / currentTask.firstNum
+//         } else {
+//             trueResult = currentTask.firstNum / currentTask.secondNum
+//         }
+//     }
+// }
+// checkResult()
+
+// enterBtn.addEventListener('click', checkResult)
+
+const wave = document.querySelector('.wave')
+// вода поднимается
+function waterUp() {
+    const drop = document.querySelector('.drop');
+    const wave = document.querySelector('.wave')
+
+console.log('Проверка');
+
+        const positionCircle = drop.getBoundingClientRect();
+        const positionWave = wave.getBoundingClientRect();
+
+        if (positionCircle.y + positionCircle.height >= positionWave.y) {
+            console.log('WaterUp');
+            wave.style.height = positionWave.height + 40 + 'px';
+            drop.remove();
+            // createDrop()
+        } 
+        if (positionWave.y <= positionWave.height ) {
+            console.log('gameeeeee overrrrrr');
+
+            gameOver()
+        }
+}
+
+const intervalPosition = setInterval(waterUp, 1000);
+
+
+function gameOver() {
+    console.log('game over');
+    // clearInterval(intervalPosition);
+    containerGameDrop.style.display = 'none';
+    containerGameOver.style.display = 'block'
+    // // clearInterval(intervalPosition); - очищает ф-ию setInterval(waterUp, 100); - чтобы она 
+    // // не проверяла позицию капли и волны + чтобы отключился звук(когда волна поднимается)
+    clearInterval(intervalPosition);
+    // gamePage.style.display = 'none';
+    // gameOverGame.style.display = 'flex';
+    // rightAnswer.textContent = rightAnswer.textContent + trueResultCount;
+    // wrongAnswer.textContent = wrongAnswer.textContent + wrongResultCount;
+    // scoreGame.textContent += score.textContent;
+    // audioSea.pause();
+}
+
+
+
+
+
+
 
 
   // Функция для переключения между полноэкранным режимом и обычным.
@@ -142,10 +303,11 @@ function toggleFullScreen(elem) {
         }
     }
 }
-
 document.getElementById('fullScreen').addEventListener('click', function() {
-    toggleFullScreen(document.getElementById('content'));
+    const content = document.getElementById('content')
+    toggleFullScreen(content);
 });
+
 
 // Music // Music // Music // Music // Music
 let music = new Audio();
